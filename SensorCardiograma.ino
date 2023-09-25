23a0533f91ac2ba10c30da88b96a59591d25df9c
Joaquín Aldunate <joaquin.aldunate@gmail.com>
2023-09-25 11:51:59 -0300

    refactor

--- SensorCardiograma.ino
+++ SensorCardiograma.ino
@@ -1,17 +1,12 @@
-#define PIN_MOTOR 9
 #define PIN_PULSE_SENSOR A0
 #define samplingRate 100
 
-float originalValue = 0;
-float lowpassed = 0;
-float _lowpassed2 = 0;
-float highpassed = 0;
-float lowpassRatio = 0.1;
-float highPassRatio = 0.1;
-int interval = 1000 / samplingRate;
-unsigned long nextMeasurement = 0;
-int motorSpeed = 0;
+#define MONITOR_SIGNALS
+#define MONITOR_RATE
+// #define DUMMY_HEARBEAT
 
+
+// struct encargada de medir la frecuencia en la señal
 struct PulseAnalyzer {
   // umbral de pulso que se auto-ajusta
   float movingThreshold = 2.;
@@ -22,19 +17,28 @@ struct PulseAnalyzer {
   unsigned long lastPositiveCross = 0;
   float measuredFrequencyHertz = 0.;
   void thresholdCrossedUp(unsigned long now) {
+    // funcion para llamar cuando el pulso cruza el umbral en dirección positiva
+    // se mide el intervalo de tiempo respecto de la última vez, y así obtenemos
+    // la frecuencia
     unsigned long deltaTimeMillis = now - lastPositiveCross;
-    measuredFrequencyHertz = 1000. / deltaTimeMillis;
+    float hz = 1000. / (float)deltaTimeMillis;
+    // promediar
+    measuredFrequencyHertz = hz * 0.3 + measuredFrequencyHertz * 0.7;
+    lastPositiveCross = now;
   }
   void inSample(float sampleValue, unsigned long now) {
     bool currentSlopePositive = sampleValue > movingThreshold;
+    // crea una línea de comparación para reconocer un pulso, aunque este varíe
     if (currentSlopePositive) {
-      movingThreshold = min(sampleValue, maxThreshold);
+      movingThreshold = movingThreshold * 0.7 + sampleValue * 0.3;
+      if (movingThreshold > maxThreshold) {
+        movingThreshold = maxThreshold;
+      };
       if (!prevSlopePositive) {
         thresholdCrossedUp(now);
       }
     } else {
-      motorSpeed = 0;
-      movingThreshold -= 40. / samplingRate;
+      movingThreshold -= 2. / samplingRate;
       if (movingThreshold < minThreshold) {
         movingThreshold = minThreshold;
       }
@@ -43,36 +47,75 @@ struct PulseAnalyzer {
   }
 } PulseAnalyzer;
 
-void measure() {
-  originalValue = analogRead(PIN_PULSE_SENSOR);
-  lowpassed = lowpassRatio * originalValue + (1 - lowpassRatio) * lowpassed;
-  _lowpassed2 = highPassRatio * lowpassed + (1 - highPassRatio) * _lowpassed2;
-  highpassed = lowpassed - _lowpassed2;
-}
+// struct encargado de filtrar la señal
+struct SignalFilter {
+
+  float originalValue = 0;
+  float lowpassed = 0;
+  float _lowpassed2 = 0;
+  float outputValue = 0;
+  float lowpassRatio = 0.1;
+  float highPassRatio = 0.1;
+
+  int interval = 1000 / samplingRate;
+  unsigned long nextMeasurement = 0;
+
+  void measure() {
+    originalValue = analogRead(PIN_PULSE_SENSOR);
+    lowpassed = lowpassRatio * originalValue + (1 - lowpassRatio) * lowpassed;
+    _lowpassed2 = highPassRatio * lowpassed + (1 - highPassRatio) * _lowpassed2;
+    outputValue = lowpassed - _lowpassed2;
+  }
+
+  // llamar ésta en cada loop
+  void loop() {
+    unsigned long now = millis();
+    if (now > nextMeasurement) {
+      nextMeasurement = now + interval;
+      measure();
+    }
+  }
+} SignalFilter;
+
 
 void setup() {
   pinMode(PIN_PULSE_SENSOR, INPUT);
-  pinMode(PIN_MOTOR, OUTPUT);
-  analogWrite(PIN_MOTOR, motorSpeed);  // Configura la velocidad inicial del motor
+#if defined(MONITOR_SIGNALS) || defined(MONITOR_RATE)
   Serial.begin(9600);
+#endif
 }
 
 void loop() {
   unsigned long now = millis();
-  if (now > nextMeasurement) {
-    nextMeasurement = now + interval;
-    measure();
-    Serial.print(-1);
-    Serial.print(',');
-    Serial.print(highpassed, 4);
-    Serial.print(',');
-    Serial.print(PulseAnalyzer.movingThreshold, 4);
-    Serial.print(',');
-    Serial.print(3);
-    Serial.println();
-    // analogWrite(motorPin, 0);
-    PulseAnalyzer.inSample(highpassed, now);
 
-    analogWrite(PIN_MOTOR, motorSpeed);  // Controla la velocidad del motor
-  }
+  SignalFilter.loop();
+
+
+  float heartSignal = SignalFilter.outputValue;
+#if defined(DUMMY_HEARBEAT)
+  float r = PI * float(now) / 1000.;
+  float s = sin(r);
+  heartSignal = 2. * s * s;
+#endif
+
+
+  PulseAnalyzer.inSample(heartSignal, now);
+
+#if defined(MONITOR_SIGNALS)
+  Serial.print(-1);
+  Serial.print(',');
+  Serial.print(heartSignal, 4);
+  Serial.print(',');
+  Serial.print(PulseAnalyzer.movingThreshold, 4);
+  Serial.print(',');
+  Serial.print(3);
+  Serial.print(',');
+#endif
+#if defined(MONITOR_RATE)
+  Serial.print(PulseAnalyzer.measuredFrequencyHertz, 4);
+  Serial.print(',');
+#endif
+#if defined(MONITOR_SIGNALS) || defined(MONITOR_RATE)
+  Serial.println();
+#endif
 }
