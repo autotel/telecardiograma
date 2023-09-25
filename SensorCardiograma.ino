cbb50244879fbcbdc7371c7dc27897a3bbf5ffbd
Joaquín Aldunate <joaquin.aldunate@gmail.com>
2023-09-25 11:08:47 -0300

    better measurement

--- SensorCardiograma.ino
+++ SensorCardiograma.ino
@@ -1,22 +1,47 @@
 #define PIN_MOTOR 9
 #define PIN_PULSE_SENSOR A0
+#define samplingRate 100
 
-
-double originalValue = 0;
-double lowpassed = 0;
-double _lowpassed2 = 0;
-double highpassed = 0;
-float lowpassRatio = 0.05;
-float highPassRatio = 0.01;
-int samplingRate = 100;
+float originalValue = 0;
+float lowpassed = 0;
+float _lowpassed2 = 0;
+float highpassed = 0;
+float lowpassRatio = 0.1;
+float highPassRatio = 0.1;
 int interval = 1000 / samplingRate;
 unsigned long nextMeasurement = 0;
 int motorSpeed = 0;
 
-// umbral de pulso que se auto-ajusta
-double movingThreshold = 2;
-// mínimo del umbral, para prevenir detectar ruido
-double minThreshold = 2;
+struct PulseAnalyzer {
+  // umbral de pulso que se auto-ajusta
+  float movingThreshold = 2.;
+  // mínimo del umbral, para prevenir detectar ruido
+  float minThreshold = 1.6;
+  float maxThreshold = 4.;
+  bool prevSlopePositive = false;
+  unsigned long lastPositiveCross = 0;
+  float measuredFrequencyHertz = 0.;
+  void thresholdCrossedUp(unsigned long now) {
+    unsigned long deltaTimeMillis = now - lastPositiveCross;
+    measuredFrequencyHertz = 1000. / deltaTimeMillis;
+  }
+  void inSample(float sampleValue, unsigned long now) {
+    bool currentSlopePositive = sampleValue > movingThreshold;
+    if (currentSlopePositive) {
+      movingThreshold = min(sampleValue, maxThreshold);
+      if (!prevSlopePositive) {
+        thresholdCrossedUp(now);
+      }
+    } else {
+      motorSpeed = 0;
+      movingThreshold -= 40. / samplingRate;
+      if (movingThreshold < minThreshold) {
+        movingThreshold = minThreshold;
+      }
+    }
+    prevSlopePositive = currentSlopePositive;
+  }
+} PulseAnalyzer;
 
 void measure() {
   originalValue = analogRead(PIN_PULSE_SENSOR);
@@ -37,20 +62,17 @@ void loop() {
   if (now > nextMeasurement) {
     nextMeasurement = now + interval;
     measure();
-    Serial.print(highpassed);
+    Serial.print(-1);
     Serial.print(',');
-    Serial.print(movingThreshold);
+    Serial.print(highpassed, 4);
+    Serial.print(',');
+    Serial.print(PulseAnalyzer.movingThreshold, 4);
+    Serial.print(',');
+    Serial.print(3);
     Serial.println();
     // analogWrite(motorPin, 0);
+    PulseAnalyzer.inSample(highpassed, now);
 
-    if (highpassed > movingThreshold) {
-      motorSpeed = 255;  // Puedes ajustar la velocidad según tus preferencias
-      movingThreshold = highpassed;
-    } else {
-      motorSpeed = 0;
-      movingThreshold *= samplingRate / 1000.00;
-    }
-
-    analogWrite(PIN_MOTOR, motorSpeed); // Controla la velocidad del motor
+    analogWrite(PIN_MOTOR, motorSpeed);  // Controla la velocidad del motor
   }
 }
