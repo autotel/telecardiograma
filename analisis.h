627cedd81462a8d3b7f1bdad04cfc173f971c818
Joaquín Aldunate <joaquin.aldunate@gmail.com>
2023-09-29 20:36:36 -0300

    adjust params, with good positions it works, not with bad positions

--- analisis.h
+++ analisis.h
@@ -18,11 +18,12 @@ struct PulseAnalyzer {
   float movingThreshold = 2.;
   // mínimo del umbral, para prevenir detectar ruido
   float minThreshold = 1.6;
-  float maxThreshold = 4.;
+  float maxThreshold = 13.;
   bool prevSlopePositive = false;
   unsigned long lastPositiveCross = 0;
   unsigned long lastMeasurement = 0;
   float measuredFrequencyHertz = 0.;
+  unsigned int stateshow = 0;
   void thresholdCrossedUp(unsigned long now, unsigned long interval) {
     // funcion para llamar cuando el pulso cruza el umbral en dirección positiva
     // se mide el intervalo de tiempo respecto de la última vez, y así obtenemos
@@ -31,6 +32,7 @@ struct PulseAnalyzer {
     float hz = 1000. / (float)deltaTimeMillis;
     measuredFrequencyHertz = hz;
     lastPositiveCross = now;
+    stateshow = 3;
   }
   void thresholdNotCrossed(unsigned long now, unsigned long interval) {
     unsigned long deltaTimeMillis = now - lastPositiveCross;
@@ -38,13 +40,14 @@ struct PulseAnalyzer {
     if (deltaTimeMillis > 1200) {
       measuredFrequencyHertz = 0;
     }
+    stateshow = 0;
   }
   void inSample(float sampleValue, unsigned long now) {
     bool currentSlopePositive = sampleValue > movingThreshold;
     unsigned long interval = now - lastMeasurement;
     // crea una línea de comparación para reconocer un pulso, aunque este varíe
     if (currentSlopePositive) {
-      movingThreshold = movingThreshold * 0.7 + sampleValue * 0.3;
+      movingThreshold = movingThreshold * 0.9 + sampleValue * 0.1;
       if (movingThreshold > maxThreshold) {
         movingThreshold = maxThreshold;
       };
@@ -54,7 +57,7 @@ struct PulseAnalyzer {
         thresholdNotCrossed(now, interval);
       }
     } else {
-      movingThreshold -= 2. / samplingRate;
+      movingThreshold *= 0.999;
       if (movingThreshold < minThreshold) {
         movingThreshold = minThreshold;
       }
@@ -72,8 +75,8 @@ struct SignalFilter {
   float lowpassed = 0;
   float _lowpassed2 = 0;
   float outputValue = 0;
-  float lowpassRatio = 10 / (float)samplingRate;
-  float highPassRatio = 10 / (float)samplingRate;
+  float lowpassRatio = 12 / (float)samplingRate;
+  float highPassRatio = 5 / (float)samplingRate;
   void measure() {
     originalValue = analogRead(PIN_PULSE_SENSOR);
     lowpassed = lowpassRatio * originalValue + (1 - lowpassRatio) * lowpassed;
@@ -117,18 +120,20 @@ void loop() {
 
 
 #if defined(MONITOR_SIGNALS)
-    Serial.print(-1);
+    Serial.print(-3);
     Serial.print(',');
     Serial.print(heartSignal);
-    //Serial.print(',');
-    //Serial.print(PulseAnalyzer.movingThreshold);
     Serial.print(',');
-    Serial.print(3);
+    Serial.print(PulseAnalyzer.movingThreshold);
+    Serial.print(',');
+    Serial.print(15);
+    Serial.print(',');
+    Serial.print(PulseAnalyzer.stateshow);
     Serial.print(',');
 #endif
 #if defined(MONITOR_RATE)
-    //Serial.print(PulseAnalyzer.measuredFrequencyHertz);
-    //Serial.print(',');
+    Serial.print(PulseAnalyzer.measuredFrequencyHertz);
+    Serial.print(',');
 #endif
 #if defined(MONITOR_SIGNALS) || defined(MONITOR_RATE)
     Serial.println();
